b4d219a36e84ed00ffe826a6a459b23831143065
NikhilMungi <nikhilmungi76@gmail.com>
2026-09-15 15:02:29 +0530

    Add files via upload

---
+++ ForLoop 4/EverySeventhNumber.java
@@ -0,0 +1,10 @@
+class EverySeventhNumber
+{
+    public static void main(String[] args)
+    {
+        for(int i = 7; i <= 200; i = i + 7)
+        {
+            System.out.println(i);
+        }
+    }
+}
